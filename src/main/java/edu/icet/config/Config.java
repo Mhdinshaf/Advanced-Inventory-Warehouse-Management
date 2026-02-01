7291e6532317f70626010db0e9e7e260a8de6321
Lagensha Rupasinghe <lagensharupasinghe47@gmail.com>
2026-02-01 18:20:29 +0530

    Feat:Add Categories,Update Categories,Delete Categories,Search Categories and getAll

--- src/main/java/edu/icet/config/Config.java
+++ src/main/java/edu/icet/config/Config.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class Config {
     @Bean
-    public ModelMapper getModelMapper(){
-        return new ModelMapper();
-    }
+        public ModelMapper getModelMapper(){
+            return new ModelMapper();
+        }
 }
---
+++ src/main/java/edu/icet/controller/CategoriesController.java
@@ -0,0 +1,41 @@
+package edu.icet.controller;
+
+import edu.icet.dto.CategoriesDTO;
+import edu.icet.service.CategoriesService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/Category")
+@RequiredArgsConstructor
+public class CategoriesController{
+
+    final CategoriesService categoriesService;
+
+    @PostMapping("/Add")
+    public void addCategories(@RequestBody CategoriesDTO categoriesDTO) {
+        categoriesService.addCategories(categoriesDTO);
+    }
+
+    @PutMapping("/Update")
+    public void updateCategories(@RequestBody CategoriesDTO categoriesDTO) {
+            categoriesService.updateCategories(categoriesDTO);
+    }
+
+    @DeleteMapping("/Delete-By-Id/{userId}")
+    public void deleteCategories(@PathVariable Integer categoryId) {
+            categoriesService.deleteCategories(categoryId);
+    }
+
+    @GetMapping("/Search-By-Id/{userId}")
+    public CategoriesDTO searchByID(@PathVariable Integer categoryId) {
+        return categoriesService.searchByID(categoryId);
+    }
+
+    @GetMapping("/GetAll")
+    public List<CategoriesDTO> getAll() {
+        return categoriesService.getAll();
+    }
+}
